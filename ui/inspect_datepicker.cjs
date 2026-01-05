70903f58b9d486fd6024faab30fe8e52874cf36b
Utai Salem <utaisalem@gmail.com>
2026-01-05 21:47:58 +0700

    chore: remove unused debug files and scripts

--- ui/inspect_datepicker.cjs
+++
@@ -1,8 +0,0 @@
-try {
-  const dp = require('./node_modules/@vuepic/vue-datepicker/dist/vue-datepicker.js');
-  console.log('Keys:', Object.keys(dp));
-  console.log('Default:', dp.default ? 'Exists' : 'Missing');
-  console.log('VueDatePicker:', dp.VueDatePicker ? 'Exists' : 'Missing');
-} catch (e) {
-  console.error(e);
-}
